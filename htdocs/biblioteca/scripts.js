cc1eb2fcf735da99e1ba4e4761da6a94dcfc24f8
Rafael9011 <152024433+Rafael9011@users.noreply.github.com>
2023-11-26 14:49:48 -0600

    Add files via upload

--- htdocs/biblioteca/scripts.js
+++ htdocs/biblioteca/scripts.js
@@ -1,17 +1,44 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const toggle = document.getElementById('toggle');
-    const menu = document.querySelector('.menu ul');
+    const shuffleButton = document.getElementById('shuffleButton');
+    const cardDeck = document.querySelector('.card-deck');
 
-    toggle.addEventListener('change', function () {
-        menu.classList.toggle('active');
-    });
+    const suits = ['♠️', '♥️', '♦️', '♣️'];
+    const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
-    // Cerrar el menú al hacer clic en un enlace (para dispositivos móviles)
-    const menuItems = document.querySelectorAll('.menu ul li a');
-    menuItems.forEach(item => {
-        item.addEventListener('click', function () {
-            menu.classList.remove('active');
-            toggle.checked = false;
-        });
+    // Función para crear una baraja de cartas
+    function createDeck() {
+        const deck = [];
+        for (let suit of suits) {
+            for (let value of values) {
+                deck.push(`${value}${suit}`);
+            }
+        }
+        return deck;
+    }
+
+    // Función para barajar las cartas
+    function shuffle(deck) {
+        for (let i = deck.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [deck[i], deck[j]] = [deck[j], deck[i]];
+        }
+    }
+
+    // Función para mostrar las cartas barajadas en el DOM
+    function displayCards(deck) {
+        cardDeck.innerHTML = '';
+        for (let card of deck) {
+            const cardElement = document.createElement('div');
+            cardElement.classList.add('card');
+            cardElement.textContent = card;
+            cardDeck.appendChild(cardElement);
+        }
+    }
+
+    // Evento click para barajar las cartas al presionar el botón
+    shuffleButton.addEventListener('click', function () {
+        const deck = createDeck();
+        shuffle(deck);
+        displayCards(deck);
     });
 });
